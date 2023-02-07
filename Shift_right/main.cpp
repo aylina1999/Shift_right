6db42df707ab277ffa4f567e60117b46799c9e12
Lugov_rr81@top-academy.ru <Lugov_rr81@top-academy.ru>
2023-02-07 22:50:01 +0300

    Add project files.

---
+++ Shift_right/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	//Сдвиг массива
+	int number_of_shifts;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int i = n - 1; i > 0; i--)
+		{
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = buffer;
+
+		Sleep(1000); 
+		system("CLS");
+		//Вывод сдвинутого массива на экран
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << tab;
+		}
+		cout << endl;
+	}
+}
